e8762b58ff73a45a186697b2f5ab05fc8dd5fc85
Luke I. Wilson <thelukaswils@gmail.com>
2019-07-02 19:45:09 -0500

    Reading and lexing files

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,8 +1,17 @@
 //! This entire source is used first for testing, and may later be turned into the CLI.
 
 use ir::*;
+use std::env::args;
+use std::fs::read_to_string;
 
 fn main() {
-	let tokens = ir::lexer::lex("one two three");
-	println!("{:#?}", tokens);
+	let argv = args().collect::<Vec<String>>();
+	if argv.len() > 1 {
+		let src = read_to_string(&argv[1]).unwrap();
+		let tokens = ir::lexer::lex(&src);
+		println!("{:#?}", tokens);
+	} else {
+		println!("fatal: no input file found");
+		std::process::exit(1);
+	}
 }
